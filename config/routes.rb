b5eb97d8ef564ec972781c8a9ab84c4a133b5e0c
s3678460 <40556454+s3678460@users.noreply.github.com>
2020-04-28 19:21:44 +1000

    Minh commit deployment

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,18 @@
 Rails.application.routes.draw do
   
   resources :users
+  get '/signup' => 'users#new'
+  post '/signup',  to: 'users#create'
+  get 'users/new'
+
   
   get 'data/new'
-  get 'users/new'
+
   root 'static_pages#home'
   get '/help' => 'static_pages#help'
   get '/about' => 'static_pages#about'
   get '/contact' => 'static_pages#contact'
-  get '/signup' => 'users#new'
-  post '/signup',  to: 'users#create'
+ 
 
   #Session
   get '/login', to: 'sessions#new'
--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -38,4 +38,17 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert flash.empty?
   end
 
+  test 'login with remembering' do
+    log_in_as(@user, remember_me:'1')
+    assert_not_empty cookies[:remember_token]
+  end
+
+  test 'login without remember' do
+    log_in_as(@user, remember_me:'1')
+    log_in_as(@user, remember_me:'0')
+    assert_empty cookies[:remember_token]
+  end
+
+  
+
 end
