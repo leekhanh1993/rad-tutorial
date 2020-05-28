1f2a1bfa7482a3d1f031bdd2e35b66982497a100
Lee Khanh <lkgame1993@gmail.com>
2020-05-28 20:12:58 +1000

    everything

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -11,8 +11,8 @@ class PasswordResetsController < ApplicationController
     if @user 
         @user.create_reset_digest
         @user.send_password_reset_email
-        # flash[:info] = "Email sent with password reset instructions"
-        flash[:info] = edit_password_reset_url(@user.reset_token, email: @user.email)
+        flash[:info] = "Email sent with password reset instructions"
+        # flash[:info] = edit_password_reset_url(@user.reset_token, email: @user.email)
         redirect_to root_url
     else
         flash.now[:danger] = "Email address not found"
